670bd036d51f6f491ee04efd8d07d2acac427d62
GeorgeWang26805031 <cool26805031@gmail.com>
2019-04-13 14:32:47 +0800

    update

---
+++ src/main/java/com/allpass/projectAAA/Do/Article.java
@@ -0,0 +1,126 @@
+package com.allpass.projectAAA.Do;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.File;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer articleId;
+    private String articleName;
+    private String postTime;
+    private String deadline;
+    private String checkedTime;
+    private Integer articleStudy;
+    private File uploadFile;
+    private Integer textNumber;
+    private Integer formulaNumber;
+    private String reviewText;
+
+
+//    public Article(
+//            String articleName,
+//            String postTime,
+//            String deadline,
+//            String checkedTime,
+//            Integer articleStudy,
+//            File uploadFile,
+//            Integer textNumber,
+//            Integer formulaNumber,
+//            String reviewText
+//    ){
+//        this.articleName=articleName;
+//        this.articleStudy=articleStudy;
+//        this.checkedTime=checkedTime;
+//        this.deadline=deadline;
+//        this.formulaNumber=formulaNumber;
+//        this.postTime=postTime;
+//        this.reviewText=reviewText;
+//        this.textNumber=textNumber;
+//        this.uploadFile=uploadFile;
+//    }
+
+    public void setUploadFile(File uploadFile) {
+        this.uploadFile = uploadFile;
+    }
+
+    public File getUploadFile() {
+        return uploadFile;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleStudy(Integer articleStudy) {
+        this.articleStudy = articleStudy;
+    }
+
+    public Integer getArticleStudy() {
+        return articleStudy;
+    }
+
+    public void setTextNumber(Integer textNumber) {
+        this.textNumber = textNumber;
+    }
+
+    public Integer getTextNumber() {
+        return textNumber;
+    }
+
+    public void setArticleName(String articleName) {
+        this.articleName = articleName;
+    }
+
+    public void setArticleName(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getArticleName() {
+        return articleName;
+    }
+
+    public void setCheckedTime(String checkedTime) {
+        this.checkedTime = checkedTime;
+    }
+
+    public String getCheckedTime() {
+        return checkedTime;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setPostTime(String postTime) {
+        this.postTime = postTime;
+    }
+
+    public String getPostTime() {
+        return postTime;
+    }
+
+    public void setFormulaNumber(Integer formulaNumber) {
+        this.formulaNumber = formulaNumber;
+    }
+
+    public Integer getFormulaNumber() {
+        return formulaNumber;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+}

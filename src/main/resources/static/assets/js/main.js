a497e7b356d6e8015db58bea4d9f586df19f6c95
Claire <claire0230025@gmal.com>
2019-05-09 22:16:02 +0800

    0509-1 新增dateRangePicker.css 修改細部js

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -91,48 +91,6 @@
 			{
 			document.getElementById("participantNumber").value = value * 3;
 			}
-
-		// // activityHold.html datepicker
-		// $( function() {
-		// 	var dateFormat = "mm/dd/yy",
-		// 		from = $( "#activitystart" )
-		// 			.datepicker({
-		// 				defaultDate: "+1w",
-		// 				changeMonth: true,
-		// 				changeYear:true,
-		// 				numberOfMonths: 2
-		// 			})
-		// 			.on( "change", function() {
-		// 				to.datepicker( "option", "minDate", getDate( this ) );
-		// 			}),
-		// 		to = $( "#activityend" ).datepicker({
-		// 			defaultDate: "+1w",
-		// 			// changeMonth: true,
-		// 			numberOfMonths: 2
-		// 		})
-		// 			.on( "change", function() {
-		// 				from.datepicker( "option", "maxDate", getDate( this ) );
-		// 			});
-		//
-		// 	function getDate( element ) {
-		// 		var date;
-		// 		try {
-		// 			date = $.datepicker.parseDate( dateFormat, element.value );
-		// 		} catch( error ) {
-		// 			date = null;
-		// 		}
-		//
-		// 		return date;
-		// 	}
-		// } );
-		// // memberRegister.html datepicker
-		// $( function() {
-		// 	$( "#datepicker" ).datepicker({
-		// 		changeMonth: true,
-		// 		changeYear: true,
-		// 	});
-		// } );
-
 	});
 })(jQuery);
 

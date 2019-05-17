eda4e9491612e59c374e43e7503404d03133b269
Claire <claire0230025@gmal.com>
2019-05-17 15:18:27 +0800

    0517

--- src/main/java/com/allpass/projectAAA/Model/Member.java
+++ src/main/java/com/allpass/projectAAA/Model/Member.java
@@ -12,8 +12,8 @@ import java.util.Set;
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "idCardNumber"))
 public class  Member  {
     @Id
-    @Column(name = "MEMBER_ID")
-    private Long id;
+//    @Column(name = "MEMBER_ID")
+    private Long member_id;
     private String name;
     private String idCardNumber;
     private String password;
@@ -52,72 +52,72 @@ public class  Member  {
     public Member(){
 
     }
-//    public Member(
-//            Long id,
-//            String name,
-//            String idCardNumber,
-//            String password,
-//            String email,
-//            String phoneNumber,
-//            Integer gender,
-//            String birthday,
-//            String special,
-//            Integer educational,
-//            Integer study,
-//            Float tokenBalance
-//    ) {
-//        this.id=id;
-//        this.name=name;
-//        this.idCardNumber=idCardNumber;
-//        this.password=password;
-//        this.email=email;
-//        this.phoneNumber=phoneNumber;
-//        this.gender=gender;
-//        this.birthday=birthday;
-//        this.educational=educational;
-//        this.study=study;
-//        this.tokenBalance=tokenBalance;
-//        this.special=special;
-//    }
-//    public Member(
-//            Long id,
-//            String name,
-//            String idCardNumber,
-//            String password,
-//            String email,
-//            String phoneNumber,
-//            Integer gender,
-//            String birthday,
-//            String special,
-//            Integer educational,
-//            Integer study,
-//            Float tokenBalance,
-//            Collection<Member_Role> roles
-//    ) {
-//        this.id=id;
-//        this.name=name;
-//        this.idCardNumber=idCardNumber;
-//        this.password=password;
-//        this.email=email;
-//        this.phoneNumber=phoneNumber;
-//        this.gender=gender;
-//        this.birthday=birthday;
-//        this.educational=educational;
-//        this.study=study;
-//        this.tokenBalance=tokenBalance;
-//        this.special=special;
-//        this.roles=roles;
-//    }
+    public Member(
+            Long id,
+            String name,
+            String idCardNumber,
+            String password,
+            String email,
+            String phoneNumber,
+            Integer gender,
+            String birthday,
+            String special,
+            Integer educational,
+            Integer study,
+            Float tokenBalance
+    ) {
+        this.member_id=id;
+        this.name=name;
+        this.idCardNumber=idCardNumber;
+        this.password=password;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.gender=gender;
+        this.birthday=birthday;
+        this.educational=educational;
+        this.study=study;
+        this.tokenBalance=tokenBalance;
+        this.special=special;
+    }
+    public Member(
+            Long id,
+            String name,
+            String idCardNumber,
+            String password,
+            String email,
+            String phoneNumber,
+            Integer gender,
+            String birthday,
+            String special,
+            Integer educational,
+            Integer study,
+            Float tokenBalance,
+            Collection<Member_Role> roles
+    ) {
+        this.member_id=id;
+        this.name=name;
+        this.idCardNumber=idCardNumber;
+        this.password=password;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.gender=gender;
+        this.birthday=birthday;
+        this.educational=educational;
+        this.study=study;
+        this.tokenBalance=tokenBalance;
+        this.special=special;
+        this.roles=roles;
+    }
 
 
 
 
 
     public void setId(Long id) {
-        this.id = id;
+        this.member_id = id;
     }
 
-    public Long getId() { return id; }
+    public Long getId() { return member_id; }
 
     public void setName(String name) {
         this.name = name;
@@ -222,7 +222,7 @@ public class  Member  {
         // Objects 有 hash() 方法可以使用
         // 以下可以簡化為 return Objects.hash(name, number);
         int hash = 7;
-        hash = 47 * hash + Objects.hashCode(this.id);
+        hash = 47 * hash + Objects.hashCode(this.member_id);
         hash = 47 * hash + Objects.hashCode(this.idCardNumber);
         return hash;
     }

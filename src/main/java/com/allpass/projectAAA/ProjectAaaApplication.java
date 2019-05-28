8ce4fe802a77fff0a4a3ca38bed9cce774d3dc06
GeorgeWang26805031 <cool26805031@gmail.com>
2019-05-28 22:26:49 +0800

    Merge branch 'Claire-html' of https://github.com/yihsiulee/projectAAA
    # Conflicts:
    #	src/main/resources/templates/formal/activity.html
    #	src/main/resources/templates/formal/activityCheckTable.html
    #	src/main/resources/templates/formal/activityHold.html
    #	src/main/resources/templates/formal/activityList.html
    #	src/main/resources/templates/formal/activityManagement.html
    #	src/main/resources/templates/formal/article.html
    #	src/main/resources/templates/formal/articleAuthorReview.html
    #	src/main/resources/templates/formal/articleAuthorReviewList.html
    #	src/main/resources/templates/formal/articlePost.html
    #	src/main/resources/templates/formal/articleReviewPost.html
    #	src/main/resources/templates/formal/index.html
    #	src/main/resources/templates/formal/memberForgot.html
    #	src/main/resources/templates/formal/memberInfo.html
    #	src/main/resources/templates/formal/memberLogin.html
    #	src/main/resources/templates/formal/memberRegister.html
    #	src/main/resources/templates/formal/qa.html

--- src/main/java/com/allpass/projectAAA/ProjectAaaApplication.java
+++ src/main/java/com/allpass/projectAAA/ProjectAaaApplication.java
@@ -113,7 +113,8 @@ public class ProjectAaaApplication  {
         }
 
 
-        List<Activity> activityList=activityService.getActivityList();
+        List<Activity> activityList=new ArrayList<>();
+        activityService.getActivityList().stream().filter(i->i.getLimitedParticipants()!=0&&i.getArticleNumber()!=0).forEach(a->activityList.add(a));
         List<String> img = new ArrayList<>();
         List<String> activity_Image = new ArrayList<>();
         for(Activity image:activityList) {
